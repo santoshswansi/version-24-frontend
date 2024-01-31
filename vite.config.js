11fa232de44346ce32cd9769880314e8baceecbd
santoshswansi <santoshswansi091@gmail.com>
2024-02-01 02:05:37 +0600

    feature: Added landing overlay

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
 });
-=======
-})
->>>>>>> 67a11be98893225acd0a626073d0c1d6e0366b33
